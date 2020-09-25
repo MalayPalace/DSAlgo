afea157f2928a9dda6246424b84cdb6a2dbbdcce
Malay Shah <malayshah182@gmail.com>
2020-09-25 12:22:34 +0530

    SortedLinkedList Implementation better performance

--- src/main/java/com/list/SortedLinkedList.java
+++ src/main/java/com/list/SortedLinkedList.java
@@ -3,23 +3,42 @@ package com.list;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
-public class SortedLinkedList<E extends Comparable<E>> extends LinkedList<E> {
+public class SortedLinkedList extends LinkedList<Long> {
 
 	private static final long serialVersionUID = 3L;
+	public int iter = 0;
 
 	@Override
-	public boolean add(E e) {
+	public boolean add(Long e) {
 		if (this.isEmpty()) {
 			super.add(e);
 			return true;
 		}
-		ListIterator<E> iterate = this.listIterator();
-		E value = null;
-		while (iterate.hasNext()) {
-			if ((value = iterate.next()) == null || value.compareTo(e) >= 1) {
-				iterate.previous();
-				iterate.add(e);
-				return true;
+		Long max = this.getLast();
+		Long min = this.getFirst();
+		long forwardDist = e - min;
+		long backwardDist = max - e;
+		boolean forwardFlag = (forwardDist <= backwardDist ? true : false);
+		Long value = null;
+		if (forwardFlag) {
+			ListIterator<Long> iterate = this.listIterator();
+			while (iterate.hasNext()) {
+				iter++;
+				if ((value = iterate.next()) == null || value.compareTo(e) >= 1) {
+					iterate.previous();
+					iterate.add(e);
+					return true;
+				}
+			}
+		} else {
+			ListIterator<Long> iterate = this.listIterator(this.size());
+			while (iterate.hasPrevious()) {
+				iter++;
+				if ((value = iterate.previous()) == null || value.compareTo(e) < 1) {
+					iterate.next();
+					iterate.add(e);
+					return true;
+				}
 			}
 		}
 		return super.add(e);
